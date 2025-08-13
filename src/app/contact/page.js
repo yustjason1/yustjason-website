62ae06dce0093afe741837c2cee2923a41dc63db
yustjason1 <jasonryust@gmail.com>
2025-08-13 16:51:45 -0400

    Added SVG icons to Contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,10 +5,22 @@ export default function Contact() {
                 <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
                 <p className="mb-4">Feel free to reach out to me via any of these channels: </p>
                 <ul className="list-none pl-5">
-                    <li className="mb-4">859 Spring Street NW, Atlanta GA 30308</li>
-                    <li className="mb-4"><a href="https://www.linkedin.com/in/jason-yust" className="text-blue-500 hover:underline">www.linkedin.com/in/jason-yust</a></li>
-                    <li className="mb-4">jasonryust@gmail.com</li>
-                    <li className="mb-4">(678) 761-5975</li>
+                    <li className="mb-4 flex items-center">
+                        <img src="/Contact/address.svg" alt="Address Icon" className="w-5 h-5 mr-3 flex-shrink-0 contact-icon" />
+                        <span>859 Spring Street NW, Atlanta GA 30308</span>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                        <img src="/Contact/internet.svg" alt="Internet Icon" className="w-5 h-5 mr-3 flex-shrink-0 contact-icon" />
+                        <a href="https://www.linkedin.com/in/jason-yust" className="text-blue-500 hover:underline">www.linkedin.com/in/jason-yust</a>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                        <img src="/Contact/email.svg" alt="Email Icon" className="w-5 h-5 mr-3 flex-shrink-0 contact-icon" />
+                        <span>jasonryust@gmail.com</span>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                        <img src="/Contact/phone.svg" alt="Phone Icon" className="w-5 h-5 mr-3 flex-shrink-0 contact-icon" />
+                        <span>(678) 761-5975</span>
+                    </li>
                 </ul>
             </div>
         </div>
